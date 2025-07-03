eea9296e7042880edac9695074c46ba465e2df5f
kovalch <nataliia.kovalchuk@liip.ch>
2025-07-03 16:47:31 +0200

    feat: Write own string_to_timedelta helper function

--- ckanext/subscribe/helpers.py
+++ ckanext/subscribe/helpers.py
@@ -1,4 +1,5 @@
 import ckan.plugins.toolkit as tk
+from datetime import timedelta
 
 
 def get_recaptcha_publickey():
@@ -12,3 +13,14 @@ def apply_recaptcha():
         tk.config.get("ckanext.subscribe.apply_recaptcha", True)
     )
     return apply_recaptcha
+
+
+def string_to_timedelta(s):
+    """
+    Convert string like '2 days' or '5 minutes' to timedelta.
+    """
+    parts = s.strip().split()
+    if len(parts) != 2:
+        raise ValueError(f"Invalid timedelta string: {s}")
+    number, unit = parts
+    return timedelta(**{unit: int(number)})
