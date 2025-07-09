d6ec5386c18b8f77ef61346088025173a332bb76
Rae Knowler <rae.knowler@liip.ch>
2025-07-09 18:24:43 +0200

    tests: Don't migrate db for activity plugin on CKAN 2.10

--- ckanext/subscribe/tests/conftest.py
+++ ckanext/subscribe/tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
+from ckan.plugins import toolkit
 
 
 @pytest.fixture
 def clean_db(reset_db, migrate_db_for):
     reset_db()
-    migrate_db_for("activity")
     migrate_db_for("subscribe")
+    if toolkit.check_ckan_version(min_version="2.11.0"):
+        migrate_db_for("activity")
